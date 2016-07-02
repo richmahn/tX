7b9000f9b1f94071c1f6c592285e0df1e84cfeb8
bspidel <bruce.spidel@gmail.com>
2016-07-02 01:42:43 -0400

    add actions to convert.sh

--- convert.sh
+++ convert.sh
@@ -1,34 +1,70 @@
 #!/bin/sh
+######################################################################
+#
+# NAME convert.sh  -  Start conversion flask server
+#
+  USAGE="convert.sh -h|--help|restart|start|stop"
+#
+######################################################################
 
-set x $(ps ax | grep -v grep | grep /var/www/vhosts/webhook/convert.py)
+case $1 in
+  -h|--help)
+    echo "Usage: $USAGE"
+    exit 0
+    ;;
 
-if [ $# -gt 2 ] ; then
-  kill $2
-fi
+  restart|stop)
+    set x $(ps ax | grep -v grep | grep /var/www/vhosts/webhook/convert.py)
 
-baseDir=/var/www/vhosts/webhook
-convDir=$baseDir/converters
-obsDir=$convDir/obs/templates
-taDir=$convDir/ta/templates
+    if [ $# -gt 2 ] ; then
+      kill $2
+    fi
+    
+    sleep 2
+    ;;
 
-[ -f convert.sh ] && cp convert.sh $baseDir && chown root:root convert.sh
-[ -f convert.py ] && cp convert.py $baseDir && chown root:root convert.py
-[ -f template.json ] && cp template.json $baseDir
+  restart|start)
+    baseDir=/var/www/vhosts/webhook
+    cd $baseDir
+    python $baseDir/convert.py 9096 gogs 2>&1 < /dev/null &
+    #python $baseDir/convert.py 9096 >/var/log/convert.log 2>&1 < /dev/null &
+    #tail -f /var/log/convert.log
+    ;;
+  
+  install) 
+    #baseDir=/var/www/vhosts/webhook
+    #cd $baseDir
+    #convDir=$baseDir/converters
+    #obsDir=$convDir/obs/templates
+    #taDir=$convDir/ta/templates
+    #[ -f convert.sh ] && cp convert.sh $baseDir && chown root:root convert.sh
+    #[ -f convert.py ] && cp convert.py $baseDir && chown root:root convert.py
+    #[ -f template.json ] && cp template.json $baseDir
+    #
+    #[ -d $obsDir ] || mkdir -p $obsDir
+    #[ -f converters/obs/convert_to_html.py ] && \
+    #    cp converters/obs/convert_to_html.py  $baseDir/converters/obs && \
+    #    cp converters/obs/templates/* $baseDir/converters/obs/templates
+    #chmod +x $baseDir/converters/obs/convert_to_html.py
+    #
+    #[ -d $taDir ] || mkdir -p $taDir
+    #[ -f converters/ta/convert_to_html.py ] && \
+    #    cp converters/ta/convert_to_html.py  $baseDir/converters/ta && \
+    #    cp converters/ta/templates/* $baseDir/converters/ta/templates
+    #chmod +x $baseDir/converters/ta/convert_to_html.py
+    ;;
 
-[ -d $obsDir ] || mkdir -p $obsDir
-[ -f converters/obs/convert_to_html.py ] && \
-    cp converters/obs/convert_to_html.py  $baseDir/converters/obs && \
-    cp converters/obs/templates/* $baseDir/converters/obs/templates
-chmod +x $baseDir/converters/obs/convert_to_html.py
+  "")
+    echo "Missing action."
+    echo "Usage: $USAGE"
+    exit
+    ;;
 
-[ -d $taDir ] || mkdir -p $taDir
-[ -f converters/ta/convert_to_html.py ] && \
-    cp converters/ta/convert_to_html.py  $baseDir/converters/ta && \
-    cp converters/ta/templates/* $baseDir/converters/ta/templates
-chmod +x $baseDir/converters/ta/convert_to_html.py
+  *)
+    echo "Invalid argument: $1)
+    echo "Usage: $USAGE"
+    exit 1
+    ;;
+esac
 
-cd $baseDir
-#python $baseDir/convert.py 9096 >/var/log/convert.log 2>&1 < /dev/null &
-python $baseDir/convert.py 9096 gogs 2>&1 < /dev/null &
-#tail -f /var/log/convert.log
 
