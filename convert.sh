8683370d1d7e4105b805bf7620db5a8cea260296
bspidel <bruce.spidel@gmail.com>
2016-07-02 03:48:00 -0400

    add init action

--- convert.sh
+++ convert.sh
@@ -3,9 +3,10 @@
 #
 # NAME convert.sh  -  Start conversion flask server
 #
-  USAGE="convert.sh -h|--help|install|log|restart|start|stop
+  USAGE="convert.sh -h|--help|init|install|log|restart|start|stop
   Where:
     -h|--help  Display this text
+    init       install, restart, log
     install    Make necessary run time directories
     log        Show current log in real time
     restart    Stop then start server process
@@ -16,10 +17,22 @@
 
 baseDir=/var/www/vhosts/conv.door43.org
 
+install() {
+  mkdir -p -v $baseDir/{data,output} 
+  chmod +x $baseDir/converters/*/*.py
+}
+
+
+log() {
+  tail -n 100 -f /var/log/convert.log ;;
+}
+
+
 start() {
   cd $baseDir
   python $baseDir/convert.py 9096 gogs >/var/log/convert.log 2>&1 < /dev/null &
 }
+
 
 stop() {
   set x $(ps ax | grep -v grep | grep $baseDir/convert.py)
@@ -38,15 +51,13 @@ case $1 in
     exit 0
     ;;
 
-  restart) stop ; start ;; 
-  stop)    stop ;; 
-  start)   start ;; 
-  log)     tail -n 100 -f /var/log/convert.log ;;
-  
-  install) 
-    mkdir -p -v $baseDir/{data,output} 
-    chmod +x $baseDir/converters/*/*.py
-    cd $baseDir
+  restart) stop    ; start ;; 
+  stop)    stop   ;; 
+  start)   start  ;; 
+  log)     log    ;;
+  init)    install ; restart ; log ;;
+  install)
+    install 
     ;;
 
   "")
